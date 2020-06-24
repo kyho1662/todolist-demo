af1d979818274eea4a199c6e7e96d6aa79861775
kyho <kyho1662@gmail.com>
2020-06-24 14:33:21 +0900

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <React.Fragment>
+                <h1>About</h1>
+                <p>This is the TodoList app v1.0.0. It is part of a React Crah course</p>
+            </React.Fragment>
+        </div>
+    )
+}
+
+export default About;
